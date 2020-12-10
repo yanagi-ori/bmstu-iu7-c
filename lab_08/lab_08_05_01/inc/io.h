0a38f6f767b0a3da69b57fd582e61547b01fc50f
Ivan Bogatyrev <yanagiori@gmail.com>
2020-12-10 11:28:10 +0300

    new input func

--- lab_08/lab_08_05_01/inc/io.h
+++ lab_08/lab_08_05_01/inc/io.h
@@ -5,6 +5,8 @@
 #ifndef LAB_08_05_01_IO_H
 #define LAB_08_05_01_IO_H
 
+int matrix_input(int **matrix, unsigned int rows, unsigned int columns);
+
 void output_matrix(int **matrix, unsigned int rows, unsigned int columns);
 
 #endif //LAB_08_05_01_IO_H
--- lab_08/lab_08_05_01/src/io.c
+++ lab_08/lab_08_05_01/src/io.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 
 #include "../inc/io.h"
+#include "../inc/errors.h"
 
 void output_matrix(int **matrix, unsigned int rows, unsigned int columns)
 {
@@ -17,4 +18,19 @@ void output_matrix(int **matrix, unsigned int rows, unsigned int columns)
         printf("\n");
     }
     printf("\n");
+}
+
+int matrix_input(int **matrix, unsigned int rows, unsigned int columns)
+{
+    for (unsigned int i = 0; i < rows; i++)
+    {
+        for (unsigned int j = 0; j < columns; j++)
+        {
+            if (scanf("%d", &matrix[i][j]) != 1)
+            {
+                return MATRIX_DATA_INPUT_ERROR;
+            }
+        }
+    }
+    return 0;
 }
