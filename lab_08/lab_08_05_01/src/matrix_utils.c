199ce7710930272a1a2e6680f3a48c8cc1b3eab0
Ivan Bogatyrev <yanagiori@gmail.com>
2021-01-09 16:31:40 +0300

    lost function

--- lab_08/lab_08_05_01/src/matrix_utils.c
+++ lab_08/lab_08_05_01/src/matrix_utils.c
@@ -176,3 +176,21 @@ int find_mult_result(int **matrix_a, int **matrix_b, unsigned int size, unsigned
     }
     return sum;
 }
+
+void identity_matrix(int **matrix, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (i == j)
+            {
+                matrix[i][j] = 1;
+            }
+            else
+            {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
