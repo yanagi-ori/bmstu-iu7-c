d1e5a54e57f4ae04ecfcea538720b9a71b653509
Ivan Bogatyrev <yanagiori@gmail.com>
2021-01-09 18:51:58 +0300

    added coord_of_min_in_matrix to header

--- lab_08/lab_08_05_01/inc/matrix_utils.h
+++ lab_08/lab_08_05_01/inc/matrix_utils.h
@@ -5,9 +5,9 @@
 #ifndef LAB_08_05_01_MATRIX_UTILS_H
 #define LAB_08_05_01_MATRIX_UTILS_H
 
-int delete_row(int **matrix, int *rows, int columns);
+void delete_row(int **matrix, int *rows, int columns);
 
-int delete_column(int **matrix, int rows, int *columns);
+void delete_column(int **matrix, int rows, int *columns);
 
 int copy_old_to_new(int **source_matrix, int **new_matrix, unsigned int rows, unsigned int cols, unsigned int target);
 
@@ -18,5 +18,7 @@ int add_new_column(int **matrix, unsigned int rows, unsigned int *columns, unsig
 int find_mult_result(int **matrix_a, int **matrix_b, unsigned int size, unsigned int x, unsigned int y);
 
 void identity_matrix(int **matrix, int size);
+
+void coord_of_min_in_matrix(int **matrix, unsigned int rows, unsigned int columns, unsigned int *x, unsigned int *y);
 
 #endif //LAB_08_05_01_MATRIX_UTILS_H
